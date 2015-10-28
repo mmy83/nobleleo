b5e9a49d6039de469b9665b7056e6e616e3ef1b8
mmy83 <mmy83@126.com>
2015-10-28 14:54:38 +0800

    set mysql connect conf

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"fmt"
 	_ "github.com/mmy83/nobleleo/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
 )
+
+func init(){
+	db_host :=beego.AppConfig.String("db_host")
+	db_user :=beego.AppConfig.String("db_user")
+	db_pass :=beego.AppConfig.String("db_pass")
+	db_name :=beego.AppConfig.String("db_name")
+	db_port :=beego.AppConfig.String("db_port")
+	db_charset :=beego.AppConfig.String("db_charset")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s",db_user,db_pass,db_host,db_port,db_name,db_charset)
+	orm.RegisterDataBase("default","mysql",dsn)
+}
 
 func main() {
 	beego.Run()
