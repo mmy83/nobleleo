596b2521cce81f034e0e9e93b07b0dcbdee93b48
mmy83 <mmy83@126.com>
2015-10-29 00:19:27 +0800

    格式化代码，修补bug

--- models/model.go
+++ models/model.go
@@ -6,9 +6,9 @@ import (
 )
 
 type User struct {
-	Id       int64  `orm:"auto;pk"`
-	Username string `orm:"size(100);index;unique"`
-	Password string `orm:"size(100)"`
+	Id       int64      `orm:"auto;pk"`
+	Username string     `orm:"size(100);index;unique"`
+	Password string     `orm:"size(100)"`
 	Contents []*Content `orm:"reverse(many)"`
 }
 
@@ -17,30 +17,30 @@ func (u *User) TableName() string {
 }
 
 type Content struct {
-	Id       int64  `orm:"auto;pk"`
-	Title    string `orm:"size(255)"`
-	Filepath string `orm:"size(500)"`
-	Content  string `orm:"type(text)"`
-	User     *User  `orm:"rel(fk)"`
-	Company *Company `orm:"rel(fk)"`
+	Id       int64    `orm:"auto;pk"`
+	Title    string   `orm:"size(255)"`
+	Filepath string   `orm:"size(500)"`
+	Content  string   `orm:"type(text);null"`
+	User     *User    `orm:"rel(fk)"`
+	Company  *Company `orm:"rel(fk)"`
 }
 
 func (c *Content) TableName() string {
 	return "contents"
 }
 
-type Company struct{
-	Id int64 `orm:"auto;pk"`
-	Symbol string `orm:"size(6)"`
-	Sname string `orm:"size(20)"`
-	Contents []*Content `orm:"reverse(many)"`	
+type Company struct {
+	Id       int64      `orm:"auto;pk"`
+	Symbol   string     `orm:"size(6)"`
+	Sname    string     `orm:"size(20)"`
+	Contents []*Content `orm:"reverse(many)"`
 }
 
-func (c *Company) TableName() string{
-	retun "companys"
+func (company *Company) TableName() string {
+	return "companys"
 }
 
 func init() {
 	db_prefix := beego.AppConfig.String("db_prefix")
-	orm.RegisterModelWithPrefix(db_prefix, new(User), new(Content),new(Company))
+	orm.RegisterModelWithPrefix(db_prefix, new(User), new(Content), new(Company))
 }
--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,10 @@
 package routers
 
 import (
-	"github.com/mmy83/nobleleo/controllers"
 	"github.com/astaxie/beego"
+	"github.com/mmy83/nobleleo/controllers"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
 }
