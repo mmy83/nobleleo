1c2bb8c110d9b69e8ed897a28a2658d4a36ea756
mmy83 <mmy83@126.com>
2015-11-03 00:08:45 +0800

    添加公司的分页显示功能

--- controllers/admin/company.go
+++ controllers/admin/company.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
+	"github.com/astaxie/beego/utils/pagination"
 	"github.com/mmy83/nobleleo/models"
 )
 
@@ -21,20 +22,28 @@ func (this *CompanyController) Store() {
 	company.Sname = this.GetString("Sname")
 	o := orm.NewOrm()
 	o.Insert(company)
-	this.Redirect("/admin/company",302)
+	this.Redirect("/admin/company", 302)
 }
 
-func (this *CompanyController) Index(){
+func (this *CompanyController) Index() {
+	perPage := 2
+	//paginator := pagination.SetPaginator(this.Ctx,perPage,int64(20))
 	var companys []models.Company
 	o := orm.NewOrm()
 	company := new(models.Company)
 	qs := o.QueryTable(company)
 	//qs.Filter("","")
-	num ,_ := qs.All(&companys)
-	fmt.Print(num,companys)
-	this.Data["companys"]=companys
-	this.TplNames="admin/company/index.tpl"
+	nums, _ := qs.Count()
+	//nums := int64(10)
+	fmt.Print(nums, companys)
+	beego.Debug(nums)
+	
+	paginator := pagination.SetPaginator(this.Ctx,perPage,nums)
+	qs = qs.Limit(perPage,paginator.Offset())
+	
+	beego.Debug(paginator.Offset())
+	qs.All(&companys)
+	this.Data["companys"] = companys
+	this.Data["paginator"] = paginator
+	this.TplNames = "admin/company/index.tpl"
 }
-
-
-
